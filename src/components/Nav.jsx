6ea81963e455f9cc171110fa992e346415c670ff
winniehoung <kiotseaeton@gmail.com>
2024-12-17 09:26:03 -0600

    profile with status and archie

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import './nav.css';
 
 function Nav() {
 
     const { authstate,logout } = useAuth();
-    const navigate=useNavigate;
+    const navigate=useNavigate();
     const onLogout=()=>{
         logout(navigate);
     }
     const onHome=()=>{
         navigate('/home');
     }
+    const onProfile=()=>{
+        navigate('/profile');
+    }
     return (
         <>
-            <h1 onClick={onHome}>ChatPad</h1>
+            <h1 onClick={onHome} className="heading">ChatPad</h1>
             <nav className="nav">
                 {
                     authstate.user.isadmin ? (
@@ -25,7 +29,7 @@ function Nav() {
                         </>
                     ) : (
                         <>
-                            <a href="/profile">Profile</a>
+                            <a onClick={onProfile}>Profile</a>
                             <a href="/contactus">Contact Us</a>
                         </>
                     )
