be9b7bc554d330e25b81f7b097e0f702d0f53dec
winniehoung <kiotseaeton@gmail.com>
2024-12-17 14:22:14 -0600

    recovering profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,8 +2,10 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import Table from "../components/Table";
 import Nav from "../components/Nav";
-
-function Profile(){
+import logo from '../logo.svg';
+import './home.css';
+import './profile.css';
+function Profile() {
 
     const { authstate } = useAuth();
     // const navigate=useNavigate();
@@ -12,25 +14,31 @@ function Profile(){
         return <Navigate to="/" />
     }
 
-    const headers = ['Title', 'Date','Status','\u{1F4E6}'];
+    const headers = ['Title', 'Date', 'Status', '\u{1F4E6}'];
     const initdata = [
-        ['Easy Bread Pudding Recipe',  '2024-12-15','Active','\u{1F4E6}'],
-        ['Zwilling Chopsticks',  '2024-12-33','Active','\u{1F4E6}'],
-        ['Staub Macaroon Dinnerware',  '2000-12-12','Inactive','\u{1F4E6}'],
-        ['Holiday Treats',  '2020-12-12','Inactive','\u{1F4E6}'],
+        ['Easy Bread Pudding Recipe', '2024-12-15', 'Active', '\u{1F4E6}'],
+        ['Zwilling Chopsticks', '2024-12-33', 'Active', '\u{1F4E6}'],
+        ['Staub Macaroon Dinnerware', '2000-12-12', 'Inactive', '\u{1F4E6}'],
+        ['Holiday Treats', '2020-12-12', 'Inactive', '\u{1F4E6}'],
     ];
 
     return (
         <div className="container">
-            <Nav/>
+            <Nav />
             <main className="main">
 
                 <div className="table">
                     <Table headers={headers} initdata={initdata} isadmin={authstate.user.isadmin} />
                 </div>
                 <div className="stats">
-                    <h3>{authstate.user.username}</h3>
-                    
+                    <div className="userinfo">
+                        <img src={logo} className="App-logo" alt="logo" />
+
+                        <div className="userdetail">
+                            <h3>{authstate.user.username}</h3>
+                            <p>2024-12-12</p>
+                        </div>
+                    </div>
                 </div>
             </main>
         </div>
