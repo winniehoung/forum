03f6b80dac6ac85a96cccdda6b56c934e4b54f90
winniehoung <kiotseaeton@gmail.com>
2024-12-18 14:38:56 -0600

    message and contactus style

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,32 +1,20 @@
-import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import { useState } from "react";
 import './form.css';
 import './contactus.css';
 import Nav from "../components/Nav";
 import Message from "../components/Message";
 
 function ContactUs() {
-
-    const navigate = useNavigate();
-
     const { authstate } = useAuth();
-    const [subject, setsubject] = useState('');
-    const [email, setemail] = useState(authstate.user.username);
-    const [msg, setmsg] = useState('');
-    const onSend = (e) => {
-        e.preventDefault();
 
-        // api call - send message
-        navigate('/home');
-
-    }
     return (
         <div className="container">
             <Nav />
             <main className="main">
-                <div className="table">
-                    <Message/>
+                <div className="table ">
+                    <div className="contactbox">
+                        <Message/>
+                    </div>
                 </div>
 
                 <div className="stats">
