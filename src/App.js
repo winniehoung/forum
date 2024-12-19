53d28c98409c91e2e9d02ccc1baae998714ed5d7
MingCheng111 <98194136+MingCheng111@users.noreply.github.com>
2024-12-18 19:45:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,53 @@
+//main component, set up routing with react-router-dom
+//import routes from pages/ and include in Switch component
+// import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import { AuthProvider } from './contexts/AuthContext';
+import Profile from './pages/Profile';
+import Users from './pages/Users';
+import UserRoute from './utils/UserRoute';
+import AdminRoute from './utils/AdminRoute';
+import ContactUs from './pages/ContactUs';
+import Message from './components/Message';
 
+function App() {
+  return (
+    <AuthProvider>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<UserRoute><Home /></UserRoute>} />
+          <Route path="/profile" element={<UserRoute><Profile /></UserRoute>}/>
+          <Route path='/users' element={<AdminRoute><Users/></AdminRoute>}/>
+          <Route path='/contactus' element={<UserRoute><ContactUs/></UserRoute>}/>
+          <Route path='/message' element={<Message/>}/>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Forum
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
