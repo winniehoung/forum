3e6e53f081dda93e702286baba013341f93332b5
MingCheng111 <98194136+MingCheng111@users.noreply.github.com>
2024-12-18 19:47:06 -0500

    Update ContactUs.jsx

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,1 +1,28 @@
 
+import { useAuth } from "../contexts/AuthContext";
+import './form.css';
+import './contactus.css';
+import Nav from "../components/Nav";
+import Message from "../components/Message";
+
+function ContactUs() {
+    const { authstate } = useAuth();
+
+    return (
+        <div className="container">
+            <Nav />
+            <main className="main">
+                <div className="table ">
+                    <div className="contactbox">
+                        <Message/>
+                    </div>
+                </div>
+
+                <div className="stats">
+                    <h3>Welcome, {authstate.user.username}</h3>
+                </div>
+            </main>
+        </div>
+    );
+}
+export default ContactUs;
